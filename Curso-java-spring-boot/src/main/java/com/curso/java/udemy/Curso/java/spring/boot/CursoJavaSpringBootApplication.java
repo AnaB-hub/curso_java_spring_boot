e789f430328248a2683218218dd5e6378c69abc7
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-15 14:02:27 -0300

    Retira códigos comentados

--- Curso-java-spring-boot/src/main/java/com/curso/java/udemy/Curso/java/spring/boot/CursoJavaSpringBootApplication.java
+++ Curso-java-spring-boot/src/main/java/com/curso/java/udemy/Curso/java/spring/boot/CursoJavaSpringBootApplication.java
@@ -18,23 +18,9 @@ public class CursoJavaSpringBootApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Iniciando o run");
-//		int valor = 4;
-
-//		for(int i = 1; i <= 10; i++) {
-//			String texto = "Texto " + i;
-//			System.out.println(texto);
-//		}
 
 		Curso curso = new Curso();
 		String tst =  curso.mostraDados();
-		
-		System.out.println("curso: " + tst);
-
-//		Aluno aluno = new Aluno();
-//		System.out.println(aluno.getNotaDoAluno());
-
-//		System.out.println("O valor é " + valor);
-//		System.out.println("Finalizando o run");
 		
 		LOG.info("Finalizando o run");
 		LOG.warn("Alerta no código");
