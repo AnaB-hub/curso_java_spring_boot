c7e538b9fb36111ab4411b8064528e21b2e49f19
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-15 15:31:01 -0300

    Cria projeto para trabalhar com database

---
+++ teste-database/src/test/java/com/curso/java/TesteDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
