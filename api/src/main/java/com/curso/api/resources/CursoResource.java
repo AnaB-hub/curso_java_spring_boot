f6c7994a12adee4ee3ec430107e94169b7e69711
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-16 16:24:37 -0300

    Criação do método POST de curso

--- api/src/main/java/com/curso/api/resources/CursoResource.java
+++ api/src/main/java/com/curso/api/resources/CursoResource.java
@@ -1,10 +1,14 @@
 package com.curso.api.resources;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +18,20 @@ import com.curso.api.service.CursoService;
 @RestController
 @RequestMapping("/curso")
 public class CursoResource {
-	
+
 	@Autowired
 	private CursoService cursoService;
-	
+
 	@GetMapping
 	public ResponseEntity<List<Curso>> getCursos() {
 		List<Curso> lista = cursoService.getCursos();
 		return ResponseEntity.ok().body(lista);
+	}
+
+	@PostMapping("/salvar")
+	public ResponseEntity<Curso> saveCurso(@RequestBody Curso curso) throws URISyntaxException {
+		Curso novoCurso = cursoService.save(curso);
+		return ResponseEntity.created(new URI(("/cursos/salvar/" + novoCurso.getId()))).body(novoCurso);
 	}
 
 }
