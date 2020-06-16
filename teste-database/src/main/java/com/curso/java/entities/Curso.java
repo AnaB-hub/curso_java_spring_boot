b445a9189ba27e2aa887a144bee73db6fbfb64b9
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-16 08:12:34 -0300

    Adiciona @GeneratedValue para que o ID seja gerado automaticamente

--- teste-database/src/main/java/com/curso/java/entities/Curso.java
+++ teste-database/src/main/java/com/curso/java/entities/Curso.java
@@ -1,6 +1,8 @@
 package com.curso.java.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,6 +11,7 @@ import javax.persistence.Table;
 public class Curso {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
